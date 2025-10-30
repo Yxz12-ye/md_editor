024e9e0d188487693ea09286d5679d463dccbb09
Yxz12-ye <3078961710@qq.com>
2025-10-30 20:58:05 +0800

    style: format code for consistency; improve spacing and alignment in MainWindow, CoreView, and WelcomeView

--- app/MainWindow.cpp
+++ app/MainWindow.cpp
@@ -3,7 +3,7 @@
 void MainWindow::initMenu()
 {
     menuBar = new ElaMenuBar(this);
-    fileMenu = new ElaMenu(tr("File"),this);
+    fileMenu = new ElaMenu(tr("File"), this);
     auto newFile_action = new QAction(tr("New File"));
     actionList.append(newFile_action);
     fileMenu->addActions(actionList);
@@ -13,16 +13,16 @@ void MainWindow::initMenu()
 
 void MainWindow::initContent()
 {
-    page = new CoreView(nullptr);
-    welcomeView = new WelcomeView(nullptr);
+    page = new CoreView(this);
+    welcomeView = new WelcomeView(page);
     this->setIsNavigationBarEnable(false);
-    page->get()->addTab(welcomeView,tr("Welcome"));
+    page->get()->addTab(welcomeView, tr("Welcome"));
     page->get()->setMinimumSize(this->size());
     page->setMinimumSize(this->size());
     this->setCentralCustomWidget(page);
 }
 
-MainWindow::MainWindow()
+MainWindow::MainWindow() : ElaWindow(this)
 {
     initMenu();
     initContent();
--- app/view/CoreView.cpp
+++ app/view/CoreView.cpp
@@ -1,12 +1,14 @@
 #include "CoreView.h"
 
-CoreView::CoreView(QWidget *parent):BaseView(parent)
+CoreView::CoreView(QWidget *parent) : BaseView(parent)
 {
     pageContiner = new ElaTabWidget(this);
 }
 
 ElaTabWidget *CoreView::get()
 {
-    if(pageContiner) return pageContiner;
-    else return nullptr;
+    if (pageContiner)
+        return pageContiner;
+    else
+        return nullptr;
 }
